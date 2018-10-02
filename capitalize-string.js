cab0e7cacf32d33bd03af921b17c9a9a6ac5334b
dbrudner <dbrudne1@gmail.com>
2018-10-01 20:38:57 -0500

    added capitalize string snippet

---
+++ capitalize-string.js
@@ -0,0 +1,7 @@
+/**
+ * Capitalize the first letter of the string and lowercase the rest
+ * @returns {String} returns a capitalized string
+ */
+function capitalize(string) {
+	return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
+}
