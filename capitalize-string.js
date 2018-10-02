d2048c1e3eaca55d09ac2d1ae76d7f75598f0efa
dbrudner <dbrudne1@gmail.com>
2018-10-01 20:40:57 -0500

    added capitalize function expression

--- capitalize-string.js
+++ capitalize-string.js
@@ -5,3 +5,6 @@
 function capitalize(string) {
 	return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
 }
+
+// As a function expression
+const capitalize = string => string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
