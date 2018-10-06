fbe4e941ba1eba88dea19fbf349ee35f6972787a
Kartik Pandey <kartikpandey2@gmail.com>
2018-10-07 03:20:40 +0530

    create express_server.js
    how to create a basic express server

---
+++ express_server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World..')
+})
+
+app.listen('3000', () => {
+  console.log('Server Started at PORT 3000')
+})
